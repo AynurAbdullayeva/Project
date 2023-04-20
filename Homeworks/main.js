5ab12c39918091cfbfd6279a26c84f26b1041e90
aynurabdullayeva <aynurabdullayeva252@gmail.com>
2023-04-20 11:33:02 +0400

    first commit

---
+++ Homeworks/main.js
@@ -0,0 +1,113 @@
+// Task 1
+
+//  function arr1(array,n){
+//     if(n==1){
+//         return array[0]
+//     }
+//     return array.slince(0,n)
+//  }
+//  console.log(arr1[23, 73, 99, 20])
+//  console.log(arr1([23, 73, 99, 20],3))
+
+
+
+
+// Task 2
+
+// let arr = [1, 73, 99, 20]
+// console.log(arr.join('*'));
+
+
+
+
+// Task 3
+
+// convert = function convert(str) {
+//     return str.replace(/([a-z]+)|([A-Z]+)/g, function (match, chr) {
+//         return chr ? match.toUpperCase() : match.toLowerCase();
+//     });
+// }
+// console.log(convert('The Quick Brown Fox'));
+
+
+
+
+// Task 4
+
+// const arr = [1, 3, false, 5, undefined, 20, null, NaN, “ ”]
+// const clear = array.filter(element => {
+//     return element;
+// });
+// console.log(clear);
+
+
+
+
+
+// Task 5
+
+// var array=[1,1,2,2,3,4,2,5,6,1,6]
+// var clearduplicate=arr.filter((x,i) => arr.indexOf(x) === i);
+
+// console.log(clearduplicate)
+
+
+
+
+// Task 6
+
+// function isEqual() {
+//     var arr1 = [1, 2, 3, 4, "hello"];
+//     var arr2 = [1, 2, 3, 4, "hello"];
+
+//     if (arr1.lengt = arr2.length)
+//         return "True";
+//     else {
+//         for (var i = 0; i < arr1.length; i++)
+//             if (arr1[i] != arr2[i])
+//                 return "False";
+//     }
+// }
+// var array = isEqual();
+// console.log(isEqual([1, 2, 3, 4, "hello"], [1, 2, 3, 4, "hello"]))
+
+
+
+
+// Task 7
+
+// const chars = []
+// let sum = -1
+// function chars(string, char) {
+//     for (let i = 0; i <= string.length; i++) {
+//         if (string[i] == -1) {
+//             sum = i;
+//         } else {
+//             sum = sum + i
+//         }
+//     }
+//     consol.log(sum)
+// }
+
+// chars("salam", "a");
+
+
+
+
+// Task8
+
+
+
+
+// Task 9
+
+// const array = arr => arr.reduce(
+//     (a, b) => a.const(Array.isArray(b) ? flatten(b) : b), []
+// );
+// console.log(array([1, [2], [3], [4], [5, 6, 7], 8, [9], [10]]).join('*'))
+
+
+
+
+
+// Task 10
